5f65409258d189402e0de5ff81efa84e86d95edf
AmirHosseinAghajari <30867537+Aghajari@users.noreply.github.com>
2019-04-07 00:35:06 +0430

    1.01

--- AmirTextButtonView/AmirTextButtonView/src/main/java/com/aghajari/amirtextbuttonview/AmirTextButtonListener.java
+++ AmirTextButtonView/AmirTextButtonView/src/main/java/com/aghajari/amirtextbuttonview/AmirTextButtonListener.java
@@ -1,10 +1,27 @@
 package com.aghajari.amirtextbuttonview;
 
+/**
+ * @author Amir Hossein Aghajari
+ */
 public interface AmirTextButtonListener {
 
     /**
      * Data : new text data if state is STATE_TEXTSHOW
      */
     void onStateChanged (int newState,AmirTextData Data);
+
+    /**
+     * icon click will call this event. return true if can submit text.
+     */
     boolean canAcceptText (CharSequence text);
+
+    /**
+     * STATE_BUTTON (ButtonClick)
+     * STATE_BUTTON2 (Button2Click)
+     * STATE_EDITTEXT (IconClick)
+     *
+     * return true if can do next step!
+     */
+    boolean buttonClick (int state);
+
 }
